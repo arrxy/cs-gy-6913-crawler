80679d174b43035ceef72a10b3771d9784d2bb79
Aritro <aritroroy1999@gmail.com>
2026-09-11 18:08:00 -0400

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+def search_engine_call(text: str):
+    pass
+
+def extract_links(text: str):
+    pass
+
+def extract_html(text: str):
+    pass
+
+def crawler(text: str):
+    pass
+
+if __name__ == '__main__':
+    text = input("Enter your search query: ")
+    crawler(text)
